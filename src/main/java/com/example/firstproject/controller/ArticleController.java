3db1063aa220d8d0eb766c844e2855efb338393a
kim junsoo <kjs201105@naver.com>
2025-01-16 20:04:08 +0900

    refactoring

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -1,19 +1,59 @@
 package com.example.firstproject.controller;
 
+import com.example.firstproject.dto.ArticleForm;
+import com.example.firstproject.entity.Article;
+import com.example.firstproject.repository.ArticleRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.Optional;
+
+@Slf4j
 @Controller
 public class ArticleController {
+    private static final Logger log = LoggerFactory.getLogger(ArticleController.class);
+    @Autowired   //스프링부트가 미리 생성해 높은 객체를 가져다가 연결해줌. 의존성 주입
+    private ArticleRepository articleRepository;
+
+
     @GetMapping("/articles/new")
     public String newArticleForm() {
         return "articles/new";
     }
 
-    @PostMapping("/articles/create")    //post방식으로 전송했으므로 postMapping 사용
-    public String createArticle(){
+    @PostMapping("/articles/create")
+    public String createArticle(ArticleForm form){   //폼 데이터를 DTO로 받기
+        log.info(form.toString());     //로깅 코드 추가
+        //System.out.println(form.toString());     //DTO에 폼 데이터가 잘 담겼는지 확인
+        //1. DTO를 엔티티로 변환
+        Article article=form.toEntity();
+        log.info(article.toString());
+        //System.out.println(article.toString());
+        //2. 라파지터리로 엔티티를 DB에 저장
+        Article saved=articleRepository.save(article);   //article 엔티티를 저장해 saved 객체에 반환
+        log.info(saved.toString());
+        //System.out.println(saved.toString());
         return "";
     }
+
+    @GetMapping("/articles/{id}")    //id는 변수로 사용됨
+    public String show(@PathVariable Long id, Model model){   //매개변수로 id 받아오기   @Pathvariable: URL 요청으로 들어온 전달값을 컨트롤러의 매개변수로 가져오는 어노테이션
+        log.info("id= "+id);    //id 잘 받았는지 확인하는 로그 찍기
+        //1. id를 조회해 데이터 가져오기
+        Article articleEntity=articleRepository.findById(id).orElse(null);   //orElse: id값으로 데이터를 찾을때 해당 id 값이없으면 null을 반환하라는 뜻
+
+        //2. 모델에 데이터 등록하기
+        model.addAttribute("article",articleEntity);
+        //3. 뷰 페이지 설정하기
+        return "articles/show";   //뷰페이지는 articles라는 디렉터리 안에 show라는 파일이 있다고 가정하고 반환
+    }
+
 
 }
--- src/main/java/com/example/firstproject/dto/ArticleForm.java
+++ src/main/java/com/example/firstproject/dto/ArticleForm.java
@@ -1,28 +1,33 @@
 package com.example.firstproject.dto;
 
+import com.example.firstproject.entity.Article;
+import lombok.AllArgsConstructor;
+import lombok.ToString;
 import org.springframework.web.bind.annotation.PostMapping;
 
+@AllArgsConstructor   //클래스 안쪽의 모든 필드, title과 content를 매개변수로 하는 생성자 자동으로 만들어짐
+@ToString
 public class ArticleForm {
     private String title;   //제목
     private String content;   //내용
 
-    public ArticleForm(String title, String content) {
-        this.title = title;
-        this.content = content;
-    }
+//    public ArticleForm(String title, String content) {
+//        this.title = title;
+//        this.content = content;
+//    }
+
 
     //데이터를 잘 받았는지 확인할 toString()메서드 추가
-    @Override
-    public String toString() {
-        return "ArticleForm{" +
-                "title='" + title + '\'' +
-                ", content='" + content + '\'' +
-                '}';
-    }
+//    @Override
+//    public String toString() {
+//        return "ArticleForm{" +
+//                "title='" + title + '\'' +
+//                ", content='" + content + '\'' +
+//                '}';
+//    }
 
-    @PostMapping("/articles/create")
-    public String createArticle(ArticleForm form){   //폼 데이터를 DTO로 받기
-        System.out.println(form.toString());     //DTO에 폼 데이터가 잘 담겼는지 확인
-        return "";
+    public Article toEntity() {
+        return new Article(null,title,content);   //폼 데이터를 담은 DTO 객체를 엔티티로 반환함
+
     }
 }
